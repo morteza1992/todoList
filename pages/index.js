ba98e519c2d2eb284edd38c011d10a3e693249d8
morteza1992 <morteza.taghizade1371@gmail.com>
2022-06-28 19:55:52 +0430

    local storage

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,27 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
-import Link from "next/link";
-import TodoList from "../components/todoList/todoList.js";
 import todos from "../redux/reducers";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
-
+import dynamic from "next/dynamic";
+const TodoList = dynamic(() => import("../components/todoList/todoList.js"), {
+    ssr: false,
+});
 let initialState = {
     list: []
 }
 const store = createStore(todos, initialState)
 
 const fillTodo = () => {
-    if (typeof window !== 'undefined') {
-        let data = JSON.parse(localStorage.getItem('todoList'))
-        store.dispatch({
-            type: "FILL_TODO", data: data.list
-        })
-    }
+    let data = JSON.parse(localStorage.getItem('todoList'))
+    store.dispatch({
+        type: "FILL_TODO", data: data.list
+    })
 }
 export default function Home() {
+    if (typeof window !== "undefined") {
+        fillTodo()
+    }
     return (
         <>
             <div className={styles.container}>
